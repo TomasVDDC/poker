4e9fee75122c99e7fab5d5a83a51a1eab0ef7a86
Tomas <tomasvddc@gmail.com>
2025-08-13 13:03:41 +0200

    Destroying a game destroys the associated player sessions

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-  validates :buy_in, numericality: {greater_than_or_equal_to: 0}
+  validates :buy_in, numericality: { greater_than_or_equal_to: 0 }
   belongs_to :club
-  has_many :player_sessions
+  has_many :player_sessions, dependent: :destroy
 end
