9e3e718c9e1ae697756432e66bdd48d4cdb64775
Tomas <tomasvddc@gmail.com>
2025-08-29 16:17:19 +0200

    rails generated authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_21_145625) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_28_214828) do
   create_table "clubs", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -48,8 +48,26 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_21_145625) do
     t.index ["club_id"], name: "index_players_on_club_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
   add_foreign_key "games", "clubs"
   add_foreign_key "player_sessions", "games"
   add_foreign_key "player_sessions", "players"
   add_foreign_key "players", "clubs"
+  add_foreign_key "sessions", "users"
 end
