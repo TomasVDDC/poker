ff48fa3934cc61d4d6ff2d20e7d6a5af13b3dbe1
Tomas <tomasvddc@gmail.com>
2025-09-08 00:08:29 +0200

    FIX: Not able to delete a player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ApplicationRecord
   belongs_to :club
-  has_many :player_sessions
+  has_many :player_sessions, dependent: :destroy
   validates :name, presence: true, uniqueness: { scope: :club_id, case_sensitive: false }
 end
